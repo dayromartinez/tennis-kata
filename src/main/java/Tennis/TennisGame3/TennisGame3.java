5022c18ad1ba9e2cabce7cda92e4b61505d83e44
Dayro Martinez <djmd96@hotmail.com>
2022-01-16 12:27:24 -0500

    Refactoring Tennis Game 3 ok!

--- src/main/java/Tennis/TennisGame3/TennisGame3.java
+++ src/main/java/Tennis/TennisGame3/TennisGame3.java
@@ -8,6 +8,7 @@ public class TennisGame3 implements TennisGame {
     private int p1;
     private String p1N;
     private String p2N;
+    private String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame3(String p1N, String p2N) {
         this.p1N = p1N;
@@ -15,25 +16,56 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
+
+        String s = "";
+        int sum = p1 + p2;
+
+        if (p1 < 4 && p2 < 4 && sum != 6) {
             s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            return case1(s);
         } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            return case2(s);
         }
     }
-    
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
-        else
-            this.p2 += 1;
-        
+
+    public String case1(String score){
+
+        if (p1 == p2){
+            score += "-All";
+        } else {
+            score += "-" + p[p2];
+        }
+        return score;
     }
 
+    public String case2(String score){
+        if (p1 == p2){
+            return "Deuce";
+        }else{
+            score = p1 > p2 ? p1N : p2N;
+            return case3(score);
+        }
+    }
+
+    public String case3(String score){
+
+        int resta = p1-p2;
+        int multiplicacion = resta * resta;
+        String resultado = "";
+
+        if (multiplicacion == 1){
+            resultado = "Advantage " + score;
+        } else {
+            resultado = "Win for " + score;
+        }
+        return resultado;
+    }
+
+    public void wonPoint(String playerName) {
+        if (playerName.equals("player1")) {
+            this.p1 += 1;
+        }else {
+            this.p2 += 1;
+        }
+    }
 }
